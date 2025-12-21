b1687404272a4642e4f2bc8de222fa511ca2e167
Dimas Azizir <dimaslaylaa@gmail.com>
2025-12-21 15:25:27 +0700

    feat: dummy data of omnichannel

---
+++ lib/data/company-key-people.ts
@@ -0,0 +1,28 @@
+import { KeyPersonListItem } from "../type/Company";
+
+export const KEY_PEOPLE_LIST: KeyPersonListItem[] = [
+  {
+    id: "1",
+    name: "James Wilson",
+    title: "Chief Executive Officer",
+    avatarUrl: "https://i.pravatar.cc/80?img=12",
+  },
+  {
+    id: "2",
+    name: "Elena Rodriguez",
+    title: "VP of Engineering",
+    avatarUrl: "https://i.pravatar.cc/80?img=32",
+  },
+  {
+    id: "3",
+    name: "Michael Chen",
+    title: "CTO & Co-Founder",
+    avatarUrl: "https://i.pravatar.cc/80?img=5",
+  },
+  {
+    id: "4",
+    name: "Sarah Jenkins",
+    title: "Chief Product Officer",
+    avatarUrl: "https://i.pravatar.cc/80?img=47",
+  },
+];
